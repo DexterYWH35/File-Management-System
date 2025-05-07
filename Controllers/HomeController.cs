314ea205c8df53857ff990c09ad66274fc77c8bb
unknown <hangyap321@gmail.com>
2025-05-07 16:19:52 +0800

    Enhancement: Replace File Directory with Recent Activity section on home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,25 +2,43 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FileManagementSystem.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using FileManagementSystem.Data;
 
 namespace FileManagementSystem.Controllers;
-
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<IdentityUser> userManager)
     {
         _logger = logger;
+        _context = context;
+        _userManager = userManager;
     }
     
     [Authorize]
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
         Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
         Response.Headers["Pragma"] = "no-cache";
         Response.Headers["Expires"] = "0";
+        
+        var user = await _userManager.GetUserAsync(User);
+        if (user != null)
+        {
+            var recentFiles = await _context.Files
+                .Where(f => f.UserId == user.Id)
+                .OrderByDescending(f => f.UploadDate)
+                .Take(5)
+                .ToListAsync();
+            
+            ViewBag.RecentFiles = recentFiles;
+        }
         
         return View();
     }
